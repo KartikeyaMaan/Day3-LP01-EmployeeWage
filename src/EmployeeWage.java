c7550daef5802d0ab181c0c8c51c35b6b7af4cd7
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-22 11:47:26 +0530

    "REFACTORED : using switch case for determining daily wage"

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,21 +2,22 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
 
-        int attendanceCheck=(int)(Math.random()*10)%2;
-        if(attendanceCheck==1)
+        int attendanceCheck = (int) (Math.random() * 10) % 2;
+        if (attendanceCheck == 1)
             System.out.println("Employee is present");
         else
             System.out.println("Employee is absent");
 
-        byte wagePerHour=20;
-        byte fullDayHour=8;
-        int dailyWage=wagePerHour*fullDayHour;
-        System.out.println("Daily Employee Wage : "+dailyWage);
-
         //0-half_time, 1-full_time
-        int employeeType=(int)(Math.random()*10)%2;
-        byte partTimeHour=4;
-        int dailyWagePartTime=wagePerHour*partTimeHour;
-        System.out.println("Daily wage of part time employee : "+dailyWagePartTime);
+        int employeeType = (int) (Math.random() * 10) % 2;
+        byte wagePerHour = 20;
+        byte fullDayHour = 8;
+        byte partTimeHour = 4;
+        int dailyWage = 0;
+        switch (employeeType) {
+            case 0 -> dailyWage = wagePerHour * partTimeHour;
+            case 1 -> dailyWage = wagePerHour * fullDayHour;
+        }
+        System.out.println("Daily wage of employee is : " + dailyWage);
     }
 }
