d8685196578f75babcb7f7b659c91667b0a56ffc
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-22 11:31:54 +0530

    "ADDED : dailyWage formula"

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,10 +1,16 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
+
         int attendanceCheck=(int)(Math.random()*10)%2;
         if(attendanceCheck==1)
             System.out.println("Employee is present");
         else
             System.out.println("Employee is absent");
+
+        byte wagePerHour=20;
+        byte fullDayHour=8;
+        int dailyWage=wagePerHour*fullDayHour;
+        System.out.println("Daily Employee Wage : "+dailyWage);
     }
 }
