a1e5c84683ce2192c5c5674e7d457746b6fffde7
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-04 17:02:26 +0530

    ADDED : code to manage multiple companies

---
+++ src/EmpWageBuilder.java
@@ -0,0 +1,45 @@
+public class EmpWageBuilder {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+
+    private int numOfCompany = 0;
+    private CompanyEmpWage[] companyEmpWageArray;
+
+    public EmpWageBuilder() {
+        companyEmpWageArray = new CompanyEmpWage[5];
+    }
+
+    private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+        numOfCompany++;
+    }
+
+    private void computeEmpWage() {
+        for (int i = 0; i < numOfCompany; i++) {
+            companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+            System.out.println(companyEmpWageArray[i]);
+        }
+    }
+
+    private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= companyEmpWage.maxHourPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) (Math.random() * 10 % 3);
+            switch (empCheck) {
+                case IS_PART_TIME -> empHrs = 4;
+                case IS_FULL_TIME -> empHrs = 8;
+            }
+            totalEmpHrs += empHrs;
+        }
+        return totalEmpHrs * companyEmpWage.empRatePerHour;
+    }
+
+    public static void main(String[] args) {
+        EmpWageBuilder empWageBuilder= new EmpWageBuilder();
+        empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+        empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+        empWageBuilder.computeEmpWage();
+    }
+}
