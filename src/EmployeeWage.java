70446dcf5a57949f894b0817fd164784e6eb684e
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-04 16:26:46 +0530

    ADDED : code to save wage for each company

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,11 +3,23 @@ public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
-    public static int computeEmployeeWage(int noOfWorkingDays, int maxHrsInMonth, int wagePerHour){
-        int totalEmpHrs = 0, totalWorkingDays = 0;
-        while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < noOfWorkingDays) {
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHourPerMonth;
+    private int totalEmpWage;
+
+    public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHourPerMonth = maxHourPerMonth;
+    }
+
+    public void computeEmpWage() {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= maxHourPerMonth && totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
-            int empHrs=0;
             int empCheck = (int) (Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME -> empHrs = 4;
@@ -15,11 +27,19 @@ public class EmployeeWage {
             }
             totalEmpHrs += empHrs;
         }
-        return totalEmpHrs * wagePerHour;
+        totalEmpWage = totalEmpHrs * empRatePerHour;
+    }
+
+    public String toString() {
+        return "Total emp wage for company: " + company + " is: " + totalEmpWage;
     }
 
     public static void main(String[] args) {
-        System.out.println("Total Emp Wage of company 1 : " + computeEmployeeWage(20, 100, 10));
-        System.out.println("Total Emp Wage of company 2 : " + computeEmployeeWage(15, 80, 20));
+        EmployeeWage dMart = new EmployeeWage("DMart", 20, 2, 10);
+        EmployeeWage reliance = new EmployeeWage("Reliance", 10, 4, 20);
+        dMart.computeEmpWage();
+        System.out.println();
+        reliance.computeEmpWage();
+        System.out.println(reliance);
     }
 }
